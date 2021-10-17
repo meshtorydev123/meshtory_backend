d4f6332b29bf0b549621548f433519a58bb38322
zameelabdulsamad <zameelabdulsamad@gmail.com>
2021-10-17 10:18:11 +0530

    h

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports={
     key:"outlook11AtOctober",
     serviceID:"VA053e3cdb6faab6f455aa0341993a3f11",
     accountSID:"AC14e3ffbab94be2219706fd6eebf0146e",
-    authToken:"318b2545dc924715ab8e3763ee079c5e"
+    authToken:"d15d34e9242f96d90cc25ba32f379eda"
 }
