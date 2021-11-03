13a485fb8c9cea29398e7f8e5971cff4edef4302
zameelabdulsamad <zameelabdulsamad@gmail.com>
2021-11-03 08:44:41 +0530

    f

--- routes/user.js
+++ routes/user.js
@@ -36,7 +36,7 @@ const upload = multer({
 
 router.route("/update/profilephoto").patch( middleware.checkToken, upload.single("img"), async (req, res) => {
     await User.findOneAndUpdate(
-        {_id: req.decoded.uid}, 
+        {_id: "6180ac8e56dc972cf02cef87"}, 
         {
             $set: {
              profilephoto: req.file.path,
