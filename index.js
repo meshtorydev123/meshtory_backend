037d22e8dae02a18c8ee0a39328582a8aab00c43
zameelabdulsamad <zameelabdulsamad@gmail.com>
2021-11-09 11:12:39 +0530

    ff

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const config = require("../config");
+const config = require("./config");
 
 const port = process.env.port || 314;
 const userRoute = require("./routes/user");
